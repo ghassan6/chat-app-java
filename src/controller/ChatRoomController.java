3771110c1e6cb88d09ef2c05830ce564c573c478
ghassan6 <ghassansalameh166@gmail.com>
2024-10-02 22:18:39 +0300

    moved the text field to another class

--- src/controller/ChatRoomController.java
+++ src/controller/ChatRoomController.java
@@ -3,34 +3,27 @@ package controller;
 import java.net.Socket;
 import java.util.ArrayList;
 
-// import org.controlsfx.control.textfield.TextFields;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 import main.CustomListCell;
 import main.SQLHelper;
 import main.User;
 
 public class ChatRoomController {
-    private String username;
+    private String currentUser;
     Socket client;
     @FXML
     private Button sendButton;
     @FXML
-    private Pane displayChat;
+    private Pane dashBoard;
     @FXML
     private TextField sendField;
     @FXML
@@ -40,46 +33,20 @@ public class ChatRoomController {
     @FXML
     private ListView<User> userListView;
 
-    public void setup(Socket client , String username) {
+    public void setup(Socket client , String currentUser) {
         this.client = client;
-        this.username = username;
-        greeting(username);
+        this.currentUser = currentUser;
+        greeting(currentUser);
     }
 
-    public void greeting(String username) {
+    public void greeting(String currentUser) {
         Label welcomeMessage = new Label();
-        welcomeMessage.setText("Welcome back: " + username);
+        welcomeMessage.setText("Welcome back: " + currentUser);
         welcomeMessage.setId("welcomeLabel");
         welcomeMessage.setLayoutX(140);
         welcomeMessage.setLayoutY(67);
 
-        displayChat.getChildren().add(welcomeMessage);
-    }
-
-    public void add(ActionEvent event ) {
-        sendMessage();
-    }
-
-    public void pressEnter(KeyEvent event) {
-        if(event.getCode() == KeyCode.ENTER && !sendField.getText().isEmpty()) {
-            sendMessage();
-            event.consume();
-        }
-    }
-
-    public void sendMessage() {
-        String message = sendField.getText();
-
-        if(!message.isEmpty()) {
-            Text text = new Text();
-            text.setText(message.trim());
-
-            HBox hBox = new HBox();
-            hBox.getChildren().add(text);
-            displayChat.getChildren().add(hBox);
-
-            sendField.clear();
-        }
+        dashBoard.getChildren().add(welcomeMessage);
     }
 
       @FXML
@@ -87,30 +54,30 @@ public class ChatRoomController {
         
         search.textProperty().addListener((observable , old , newValue) -> {
 
-            if(!newValue.isEmpty()) updateSuggestions(newValue , username);
+            if(!newValue.isEmpty()) updateSuggestions(newValue , currentUser);
             
             if(newValue.isEmpty()) {
                 userListView.setVisible(false);
                 userListView.setManaged(false);
             }
-           
         });
 
         userListView.setCellFactory(param -> {
             
             CustomListCell cell = new CustomListCell();
-            cell.setCurrentUSer(SQLHelper.getUser(username));
+            cell.setCurrentUSer(SQLHelper.getUser(currentUser), client);
             return cell;
         });
       }
 
-    //   do it in thr background - runlater - thread or task
-    public void updateSuggestions(String lookForName , String currentUser) {
+    //   do it in the background 
+    public void updateSuggestions(String targetUser , String currentUser) {
 
         new Thread(() -> {
+            // look for the user in the daabase excluding the current user
+            ArrayList<User> users = SQLHelper.lookUp(targetUser , currentUser);
 
-            ArrayList<User> users = SQLHelper.lookUp(lookForName , currentUser);
-
+            // convert the user arraylist to an abservableList and show it in the ListView
             ObservableList<User> suggestions = FXCollections.observableArrayList(users);
 
             Platform.runLater(() -> {
@@ -124,6 +91,5 @@ public class ChatRoomController {
                 }
             });
         }).start();
-       
     }
 }
